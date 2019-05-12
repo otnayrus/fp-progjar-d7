51f741e46231c7f89c9276a215a04c5320697507
Indra <vinsensiusindra@gmail.com>
2019-05-12 16:51:27 +0700

    add game mechanism

--- server.py
+++ server.py
@@ -17,8 +17,7 @@ server.listen(10)
 list_of_clients = ['']  # Berisi IP dan Port Klien mulai dari index 1
 roles = ['']  # Berisi Role dari Klien mulai dari index 1
 client_names = ['']  # Berisi Nama Klien mulai dari index 1
-tally = [0, 0, 0, 0, 0, 0, 0, 0, 0] # Index ke-0 tidak dipakai
-queue_act = []
+tally = [0, 0, 0, 0, 0, 0, 0, 0, 0] # Index ke-0 tidak dipakai 
 start_game = False
 
 list_of_sides = ["villager", "werewolf", "villager", "seer", "villager", "werewolf", "villager", "villager"] 
@@ -27,7 +26,7 @@ list_of_sides = ["villager", "werewolf", "villager", "seer", "villager", "werewo
 while not start_game:
     conn, addr = server.accept()
     list_of_clients.append(conn)
-    print(str(addr[1]) + " connected")
+    print(str(addr) + " connected")
     start_new_thread(clientthread, (conn, addr))
 
 werewolfGame()
@@ -35,19 +34,54 @@ conn.close()
 server.close()
 
 
-# Game mechanism starts
+# Game mechanism here
 def werewolfGame():
     roleRandomizer()
     while True:
-        # To do game
-        return
+        # Afternoon Phase
+        broadcast("afternoon", "Village at a Day, you are headed to the assembly.", '')
+        time.sleep(60)
+        # Voting Phase
+        broadcast("voting", "Vote a player to execute!", '')
+        time.sleep(30)
+        revise("villager vote")
+        # Night Phase
+        broadcast("night", "The darkness comes, you are in your home with candle lights.", '')
+        time.sleep(20)
+        revise("werewolf vote")
+    return
         
+
+def revise(arg):
+    if arg.startswith('v'):
+        chosen = [i for i, x in enumerate(tally) if x == max(tally)]
+        if len(chosen) == 1:
+            # Execute and reveal
+            roles[chosen] = "Ded"
+            broadcast("execute", "REVEAL message", '') # To do <<<
+    elif arg.startswith('w'):
+        chosen = [i for i, x in enumerate(tally) if x == max(tally)]
+        if len(chosen) == 1:
+            # Execute and reveal
+            roles[chosen] = "Ded"
+            broadcast("kill", "Werewolf KILLS message", '') # To do <<<
+    werewolf_left = [i for i, x in enumerate(tally) if x == "werewolf"]
+    if len(roles) <= len(werewolf_left) + 1 :
+        # Werewolf win <<
+        broadcast("werewolfwin", "Werewolf won", '')
+    elif len(werewolf_left) == 0:
+        # Villager win <<
+            broadcast("villagerwin", "Villager won", '')
+
+    tallyReset()
+
 
 def roleRandomizer():
     num_participant = len(list_of_clients) - 1
     roles = list_of_sides[:num_participant]
     random.shuffle(roles)
     roles[1:], roles[0] = roles[0:], ''
+    # >>> Broadcast role <<<
 
 def tallyReset():
     for i in range(len(tally)):
@@ -67,33 +101,33 @@ def clientthread(conn, addr):
                 start_game = True
             # Jika mengirim chat pada siang hari
             elif message[0] == "chat":
-                broadcast(message[1], conn)
+                broadcast("chat", message[1], conn)
             # Voting siang
             elif message[0] == "vote":
                 target = int(message[1])
                 tally[target] += 1
-                broadcast(client_names[list_of_clients.index(conn)] + ' (' + str(list_of_clients.index(conn)) + 
+                broadcast("vote", client_names[list_of_clients.index(conn)] + ' (' + str(list_of_clients.index(conn)) + 
                     ') agreed to execute ' + client_names[target] + ' (' + str(target) + ')\n', conn)
             # Seer Ability
-            elif message[0] == "seerability":
+            elif message[0] == "seer":
                 target = int(message[1])
                 message_to_seer = ["seer", str(target) + ' is ' + str(roles[target - 1])]
-                conn.send(marshal.dumps(message_to_seer))
+                to_client("seer", message_to_seer, conn)
             else:
                 remove(conn)
         except:
             continue
 
 
-def to_client(message,connection):
-    message_to_send = ["chat", message]
+def to_client(msg_type, message,connection):
+    message_to_send = [msg_type, message]
     connection.send(marshal.dumps(message_to_send))
 
-def broadcast(message, connection):
+def broadcast(msg_type, message, connection):
     for clients in list_of_clients:
         if clients != connection:
             try:
-                message_to_send = ["chat", message]
+                message_to_send = [msg_type, message]
                 clients.send(marshal.dumps(message_to_send))
             except:
                 clients.close()
